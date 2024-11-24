f10f0c0c3b1f4d07bc696a86649e4290fadd8a8d
Muhamed-Ragab <mohamedragab1649@gmail.com>
2024-11-24 20:42:23 +0200

    remove unused and border-border style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["./assets/css/tailwind.css", "./assets/css/icons.css"],
+  css: [
+    "~/assets/css/tailwind.css",
+    "~/assets/css/icons.css",
+    "~/assets/css/BisConnectFont-v4.8/style.css",
+  ],
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
